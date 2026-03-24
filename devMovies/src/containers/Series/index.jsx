8630a6cfe47f706f41e5a77b38e97f9e80a6b65e
Caio do Carmo <Caio.carmo0521@gmail.com>
2026-03-24 14:31:58 -0300

    Projeto devMovies

---
+++ devMovies/src/containers/Series/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Series () {
+    return (
+        <div>
+            <h1>Series</h1>
+            <p>Essa é a de Series</p>
+        </div>
+    )
+}
+
+export default Series
---
+++ devMovies/src/routes/routes.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from '../containers/home'
+import Movies from '../containers/Movies'
+import Series from '../containers/Series'
+
+function Router () {
+
+    return (
+        <Routes>
+            <Route path='/' element={ <Home /> }/>
+            <Route path='/filmes' element={ <Movies /> }/>
+            <Route path='/series' element={ <Series /> }/>
+        </Routes>
+    )
+
+}
+
+export default Router
